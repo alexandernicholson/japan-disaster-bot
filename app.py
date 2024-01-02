fdb5871831b932298c637f0958687af4ac1c1628
Alexander Nicholson 4584443+DragonStuff@users.noreply.github.com <4584443+DragonStuff@users.noreply.github.com>
2024-01-03 01:16:46 +0900

    feat: update db

--- app.py
+++ app.py
@@ -8,9 +8,12 @@ import os
 import requests
 import json
 from bs4 import BeautifulSoup
+from huey import SqliteHuey, crontab
 
 # Use an env variable to store the Slack API token.
 webhook_url = os.environ['SLACK_WEBHOOK_URL']
+
+huey = SqliteHuey(filename='huey.db')
 
 # To make things super simple, we will use the Slack Incoming Webhooks API to post to Slack.
 # https://api.slack.com/incoming-webhooks
@@ -32,20 +35,34 @@ if not os.path.exists('posts.db'):
             ''')
     conn.commit()
 
-# The posts are in a JSON format and at https://unnerv.jp/api/v1/accounts/1/statuses, so we need to fetch them with requests.
-r = requests.get('https://unnerv.jp/api/v1/accounts/1/statuses')
-data = json.loads(r.text)
+def get_post_content(post):
+    # Use BeautifulSoup to parse the HTML.
+    soup = BeautifulSoup(post['content'], 'html.parser')
+    # The post content is in the <p> tag.
+    return soup.text
 
-conn = sqlite3.connect('posts.db')
-c = conn.cursor()
+@huey.periodic_task(crontab(minute='*'), retries=2, retry_delay=60)
+def update_posts():
+    # The posts are in a JSON format and at https://unnerv.jp/api/v1/accounts/1/statuses, so we need to fetch them with requests.
+    r = requests.get('https://unnerv.jp/api/v1/accounts/1/statuses')
+    data = json.loads(r.text)
 
-for post in data:
-    c.execute('SELECT * FROM posts WHERE internal_id=?', (post['id'],))
-    if c.fetchone() is None:
-        print(f'Posting {post["id"]} to Slack.')
-        # Remove the HTML tags from the post content using Markdown.
-        #post_to_slack(post['content'] + " " + post['url'])
-        c.execute('INSERT INTO posts (internal_id, url, content) VALUES (?, ?, ?)', (post['id'], post['url'], post['content']))
-        conn.commit()
+    conn = sqlite3.connect('posts.db')
+    c = conn.cursor()
 
-conn.close()
+    for post in data:
+        c.execute('SELECT * FROM posts WHERE internal_id=?', (post['id'],))
+        if c.fetchone() is None:
+            print(f'Posting {post["id"]} to Slack.')
+            clean_content = get_post_content(post)
+            # Remove the HTML tags from the post content using Markdown.
+            post_to_slack(f'{clean_content} - {post["url"]}')
+            c.execute('INSERT INTO posts (internal_id, url, content) VALUES (?, ?, ?)', (post['id'], post['url'], post['content']))
+            conn.commit()
+
+    conn.close()
+
+    return True
+
+if __name__ == '__main__':
+    update_posts()
